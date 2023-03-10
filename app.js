78f493eb5cc7d62c0ba3a4a2f874dd664c852076
RaphaelGuarim <raphaguarim@gmail.com>
2023-03-10 10:40:17 +0100

    update

--- app.js
+++ app.js
@@ -16,6 +16,30 @@ app.get('/secret', (req, res) => {
     res.end("Vous ne possédez pas les droits pour accéder à ma page secrète")
 })
 
+app.get('/error', (req, res) => {
+    res.statusCode =500
+    res.end(JSON.stringify({ message: 'Internal Server Error' }));
+})
+
+
+app.get('/img', (req, res) => {
+    res.send('<img src="https://wallpaperaccess.com/full/1283583.jpg">')
+})
+
+app.get('/redirectMe', (req, res) => {
+    res.statusCode = 302;
+    res.setHeader('Location', 'https://www.iut-littoral.fr/');
+    res.end();
+})
+
+app.get('/users/:name', (req, res) => {
+    res.end("Bienvenue sur le site de : "+req.params.name)
+})
+
+app.get('/somme', (req, res) => {
+    
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
